a1324935d970c3c605c88168ddd53877e0ea2c02
efrain <efrainhost26@gmail.com>
2024-04-15 01:42:02 -0300

    exponencial casi terminada

--- TP2SIM.py
+++ TP2SIM.py
@@ -3,7 +3,7 @@ from tkinter import ttk, messagebox
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from scipy.stats import chi2
+from scipy.stats import chi2, expon
 from collections import Counter
 
 
@@ -17,7 +17,7 @@ class GeneradorDeRandoms:
         self.notebook.pack(fill=tk.BOTH, expand=True)
 
         self.tab1 = ttk.Frame(self.notebook)
-        self.notebook.add(self.tab1, text="Normal")
+        self.notebook.add(self.tab1, text="Uniforme")
 
         self.label_N = ttk.Label(self.tab1, text="Cantidad de muestras (N):")
         self.label_N.grid(row=1, column=0, padx=10, pady=5, sticky="w")
@@ -58,16 +58,22 @@ class GeneradorDeRandoms:
         self.tab2 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab2, text="Exponencial")
 
-        self.label_N = ttk.Label(self.tab2, text="Cantidad de muestras (N):")
-        self.label_N.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-        self.entry_N = ttk.Entry(self.tab2)
-        self.entry_N.grid(row=0, column=1, padx=10, pady=5)
+        self.label_N2 = ttk.Label(self.tab2, text="Cantidad de muestras (N):")
+        self.label_N2.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+        self.entry_N2 = ttk.Entry(self.tab2)
+        self.entry_N2.grid(row=0, column=1, padx=10, pady=5)
 
         self.label_L = ttk.Label(self.tab2, text="Lambda:")
         self.label_L.grid(row=1, column=0, padx=10, pady=5, sticky="w")
 
         self.entry_L = ttk.Entry(self.tab2)
         self.entry_L.grid(row=1, column=1, padx=10, pady=5)
+
+        self.label_intervalos2 = ttk.Label(self.tab2, text="Numero de intervalos")
+        self.label_intervalos2.grid(row=2, column=0, padx=10, pady=5, sticky="w")
+
+        self.entry_intervals2 = ttk.Entry(self.tab2)
+        self.entry_intervals2.grid(row=2, column=1, padx=10, pady=5)
 
         self.button_generate = ttk.Button(
             self.tab2, text="Generar", command=self.generate_random_numbers)
@@ -77,17 +83,17 @@ class GeneradorDeRandoms:
         self.canvas = tk.Canvas(self.tab2, width=600, height=400)
         self.canvas.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
-        self.text_output = tk.Text(self.tab2, width=50, height=10)
-        self.text_output.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+        self.text_output2 = tk.Text(self.tab2, width=50, height=10)
+        self.text_output2.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
 
         # PESTAÑA 3
         self.tab3 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab3, text="Normal")
 
-        self.label_N = ttk.Label(self.tab3, text="Cantidad de muestras (N):")
-        self.label_N.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-        self.entry_N = ttk.Entry(self.tab3)
-        self.entry_N.grid(row=0, column=1, padx=10, pady=5)
+        self.label_N3 = ttk.Label(self.tab3, text="Cantidad de muestras (N):")
+        self.label_N3.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+        self.entry_N3 = ttk.Entry(self.tab3)
+        self.entry_N3.grid(row=0, column=1, padx=10, pady=5)
 
         self.button_generate = ttk.Button(
             self.tab3, text="Generar", command=self.generate_random_numbers)
@@ -97,17 +103,17 @@ class GeneradorDeRandoms:
         self.canvas = tk.Canvas(self.tab3, width=600, height=400)
         self.canvas.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
-        self.text_output = tk.Text(self.tab3, width=50, height=10)
-        self.text_output.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+        self.text_output3 = tk.Text(self.tab3, width=50, height=10)
+        self.text_output3.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
 
         # PESTAÑA 4
         self.tab4 = ttk.Frame(self.notebook)
         self.notebook.add(self.tab4, text="Poisson")
 
-        self.label_N = ttk.Label(self.tab4, text="Cantidad de muestras (N):")
-        self.label_N.grid(row=0, column=0, padx=10, pady=5, sticky="w")
-        self.entry_N = ttk.Entry(self.tab4)
-        self.entry_N.grid(row=0, column=1, padx=10, pady=5)
+        self.label_N4 = ttk.Label(self.tab4, text="Cantidad de muestras (N):")
+        self.label_N4.grid(row=0, column=0, padx=10, pady=5, sticky="w")
+        self.entry_N4 = ttk.Entry(self.tab4)
+        self.entry_N4.grid(row=0, column=1, padx=10, pady=5)
 
         self.button_generate = ttk.Button(
             self.tab4, text="Generar", command=self.generate_random_numbers)
@@ -117,23 +123,22 @@ class GeneradorDeRandoms:
         self.canvas = tk.Canvas(self.tab4, width=600, height=400)
         self.canvas.grid(row=6, column=0, columnspan=2, padx=10, pady=10)
 
-        self.text_output = tk.Text(self.tab4, width=50, height=10)
-        self.text_output.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
+        self.text_output4 = tk.Text(self.tab4, width=50, height=10)
+        self.text_output4.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
 
     def generate_random_numbers(self):
         self.text_output.delete(1.0, tk.END)  # Limpiar el widget de texto
         current_tab = self.notebook.index("current")
-        print("El usuario está en la pestaña:", current_tab)
 
+        
 
-
-        N = int(self.entry_N.get())
-        if N > 1000000:
-            messagebox.showerror(
-                "Error", "La cantidad de muestras (N) no puede ser mayor a 1,000,000.")
-            return
-
+        # Uniforme
         if current_tab == 0:
+            N = int(self.entry_N.get())
+            if N > 1000000:
+                messagebox.showerror(
+                    "Error", "La cantidad de muestras (N) no puede ser mayor a 1,000,000.")
+                return
             try:
                 A = float(self.entry_A.get())
                 B = float(self.entry_B.get())
@@ -218,15 +223,22 @@ class GeneradorDeRandoms:
             self.text_output.insert(
                 tk.END, f"Ji cuadrado de tabla: {ji_cuadrado_tabla:.4f}\n")
             self.text_output.insert(tk.END, resultado_prueba)
+
+        # Exponencial
         elif current_tab == 1:
+            N = int(self.entry_N2.get())
+            if N > 1000000:
+                messagebox.showerror(
+                    "Error", "La cantidad de muestras (N) no puede ser mayor a 1,000,000.")
+                return
             if self.entry_L.get() is ValueError:
                 messagebox.showerror(
                     "Error", "Lambda debe ser entero.")
             
-            random_numbers = np.random.exponential(scale=1/int(self.entry_L.get()), size=int(self.entry_N.get()))
+            random_numbers = np.random.exponential(scale=1/int(self.entry_L.get()), size=int(self.entry_N2.get()))
 
             try:
-                num_intervals = int(self.entry_intervals.get())
+                num_intervals = int(self.entry_intervals2.get())
                 if num_intervals not in [10, 15, 20, 25]:
                     messagebox.showerror(
                         "Error", "El número de intervalos debe ser 10, 15, 20 o 25.")
@@ -245,16 +257,42 @@ class GeneradorDeRandoms:
                                   amplitud for i in range(num_intervals)]
             limites_superiores = [
                 lim_inf + amplitud for lim_inf in limites_inferiores]
-            media = np.half(random_numbers) #REVISAR
+            media = np.mean(random_numbers) #calculo media
 
-            # Calcular frecuencias observadas
+            def densidad_probabilidad_ls(LS, lambd):
+                return 1 - np.exp(-lambd * LS)
+
+            def densidad_probabilidad_li(Li, lambd):
+                return 1 - np.exp(-lambd * Li)
+
+            # Calcular frecuencias observadas IGUAL QUE EN UNIFORME
             freq_observadas, _ = np.histogram(
                 random_numbers, bins=num_intervals)
+            
+            
+            # Calcular frecuencia esperada BIEN
+            freq_esperada = [(densidad_probabilidad_ls(limites_superiores[i], int(self.entry_L.get())) - densidad_probabilidad_li((limites_inferiores[i]),int(self.entry_L.get()))) * int(self.entry_N2.get()) for i in range(num_intervals)]
+            intervalos_removidos = 0
+            remover = 0
+            for i in freq_esperada:
+                print(i)
+            
+       
+            i = 0
+            while i < len(freq_esperada):
+                while freq_esperada[i] < 5:
+                    if i == len(freq_esperada) - 1:
+                        print(freq_esperada)
+                        print("gol")
+                        freq_esperada[i-1] += freq_esperada[i]
+                        freq_esperada.pop(i)
+                        break
+                    elif i+1 < len(freq_esperada):
+                        freq_esperada[i] += freq_esperada[i+1]
+                        freq_esperada.pop(i+1)
+                i += 1
 
-            # Calcular frecuencia esperada
-            freq_esperada = N / num_intervals
-
-            # Calcular estadístico de Ji cuadrado
+            # Calcular estadístico de Ji cuadrado BIEN
             ji_cuadrado_calculado = np.sum(
                 (freq_observadas - freq_esperada) ** 2 / freq_esperada)
 
@@ -280,15 +318,15 @@ class GeneradorDeRandoms:
             plt.show()
 
             # Mostrar resultados en el widget de texto
-            self.text_output.insert(
+            self.text_output2.insert(
                 tk.END, "Resultados del test de Ji cuadrado:\n")
-            self.text_output.insert(
+            self.text_output2.insert(
                 tk.END, f"Grados de libertad: {grados_libertad}\n")
-            self.text_output.insert(
+            self.text_output2.insert(
                 tk.END, f"Ji cuadrado calculado: {ji_cuadrado_calculado:.4f}\n")
-            self.text_output.insert(
+            self.text_output2.insert(
                 tk.END, f"Ji cuadrado de tabla: {ji_cuadrado_tabla:.4f}\n")
-            self.text_output.insert(tk.END, resultado_prueba)
+            self.text_output2.insert(tk.END, resultado_prueba)
 
         elif current_tab == 2:
             pass
